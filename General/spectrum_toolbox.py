222c10cb6f5240b32fca53a5c3f9fa88ce115b0f
Pierre Clavier <pierre.clavier.c@gmail.com>
2021-03-23 11:28:17 +0100

    functions for mismatch

--- General/spectrum_toolbox.py
+++ General/spectrum_toolbox.py
@@ -59,11 +59,85 @@ def Spectrum_embedding(sequences,k,preindex = None):
     
     embedding = sp.lil_matrix((n_sequences,4**k))
 
-    for i in range(n_sequences):
-        for w in range(len(sequences[i])-k+1):
+    for i in range(n_sequences):   # cvery sequence
+        for w in range(len(sequences[i])-k+1): # every pattern w 
             j = int(preindex[sequences[i][w:w+k]])
             embedding[i,j] += 1
         
     embedding = embedding.tocsr()
     
     return(embedding)
+
+
+def Mismatch_embedding(sequences,k,m,preindex = None):
+    """
+    Compute the spectrum embedding of a list of sequences
+
+    Parameters
+    ----------
+    sequences : string
+    k : integer
+    preindex : None or dictionnary, optional
+        If None, preindex is computed in the function. The default is None.
+
+    Returns
+    -------
+    Sparse matrix
+
+    """
+    if preindex == None:
+        preindex = preindexation(k)
+    n_sequences = sequences.shape[0]
+    
+    embedding = sp.lil_matrix((n_sequences,4**k))
+
+    for i in range(n_sequences):   # cvery sequence
+        if i%500==0:
+            print(i)
+        for w in range(len(sequences[i])-k+1): # every pattern w 
+            for j, b in enumerate(list(preindex.keys())):  # for all element of the
+                embedding[i,j] += sum(1 for a, c in zip(sequences[i][w:w+k],b) if a != c)<=m
+            
+    embedding = embedding.tocsr()
+    
+    return(embedding)
+###################################################
+#import sys
+# import pandas as pd
+# import numpy as np
+# import os
+# from tqdm import tqdm
+# os.chdir(r"C:\Users\pierr\Desktop\MVA\Kernel\kaggle\mva-KMML-challenge\data")
+
+# X = pd.read_csv('Xtr0.csv')
+# y = pd.read_csv('ytr0.csv')
+# X.set_index('Id',inplace = True)
+
+# X.head()
+# ######################################
+# params={}
+# params["k"]=4
+# preindex = preindexation(params['k'])
+# X_emb = Spectrum_embedding(X['seq'].to_numpy(),params['k'],preindex = preindex)
+# print(X_emb[0])
+
+# #####################""
+# params={}
+# params["k"]=4
+# params["m"]=1
+
+# preindex = preindexation(params['k'])
+# X_emb = Mismatch_embedding(X['seq'].to_numpy(),params['k'],params["m"],preindex = preindex)
+# print(X_emb[0])
+
+
+
+
+
+
+
+
+
+
+
+
